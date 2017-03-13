96626419e63de3220c21a438a0487d8dd83d1de1
Андрей Ждановских <andrew72ru@gmail.com>
2017-03-13 16:06:40 +0500

    Unit test of app and module, extend Url Walk Test (passed)

--- tests/functional/UrlWalkCest.php
+++ tests/functional/UrlWalkCest.php
@@ -19,6 +19,7 @@ class UrlWalkCest
     public function tryToModuleUrl(FunctionalTester $I)
     {
         $I->amOnPage(Url::toRoute(['/seotag/main']));
+        $I->see(Yii::t('app.seotag', 'No one page has a meta-tags'));
     }
 
     /**
@@ -27,5 +28,6 @@ class UrlWalkCest
     public function tryToKeywordsUrl(FunctionalTester $I)
     {
         $I->amOnPage(Url::toRoute(['/seotag/keywords']));
+        $I->seeElement('div.box-body');
     }
 }
---
+++ tests/unit/MainTest.php
@@ -0,0 +1,49 @@
+<?php
+
+
+use andrew72ru\seotag\Module;
+use andrew72ru\seotag\widgets\metaTags;
+
+class MainTest extends \Codeception\Test\Unit
+{
+    /**
+     * @var \UnitTester
+     */
+    protected $tester;
+
+    protected function _before()
+    {
+    }
+
+    protected function _after()
+    {
+    }
+
+    public function testApplication()
+    {
+        $this->tester->assertInstanceOf(\yii\base\Application::className(), Yii::$app);
+    }
+
+    // tests
+    public function testModuleIsLoadable()
+    {
+        $this->tester->assertInstanceOf(Module::className(), Yii::$app->getModule('seotag'));
+    }
+
+    public function testModuleHasUrlManager()
+    {
+        /** @var Module $module */
+        $module = Yii::$app->getModule('seotag');
+        $this->tester->assertInstanceOf(\yii\web\UrlManager::className(), $module->urlManagerComponent);
+        $this->tester->amOnPage($module->urlManagerComponent->createAbsoluteUrl('/seotag/main'));
+        $this->tester->see(Yii::t('app.seotag', 'No one page has a meta-tags'));
+    }
+
+    public function testWidgetIsLoadableAndHasModule()
+    {
+        $this->tester->wantTo('Test widget');
+        $widget = new metaTags();
+        $this->tester->assertInstanceOf('andrew72ru\seotag\widgets\metaTags', $widget);
+        $this->tester->assertInstanceOf(Module::className(), $widget->module);
+    }
+}
